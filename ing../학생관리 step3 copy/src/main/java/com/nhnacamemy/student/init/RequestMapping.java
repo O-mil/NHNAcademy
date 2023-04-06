b5b93e343022f0915a7b2d7f2347966a90624fed
O'mil <ghkwjd5343@gmail.com>
2023-04-06 18:43:05 +0900

    ServletContainerInitializer 구현

---
+++ ing../학생관리 step3 copy/src/main/java/com/nhnacamemy/student/init/RequestMapping.java
@@ -0,0 +1,17 @@
+package com.nhnacamemy.student.init;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value={ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface RequestMapping {
+    enum Method{
+        POST,GET
+    }
+    String value();
+    Method method() default Method.GET;
+}
