ac5db2dcae54b512f297a87e7f08c1bd38ad2c37
O'mil <ghkwjd5343@gmail.com>
2023-04-06 18:43:47 +0900

    .

--- ing../Student/src/main/java/com/nhnacamemy/student/exception/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.nhnacamemy.student.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
-
