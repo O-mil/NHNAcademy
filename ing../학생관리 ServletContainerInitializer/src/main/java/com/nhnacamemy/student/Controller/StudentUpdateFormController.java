21196332a59ce191c6a9131dc4bb3ab876b92792
O'mil <ghkwjd5343@gmail.com>
2023-04-06 23:16:37 +0900

    학생관리 ServletContainerInitializer

---
+++ ing../학생관리 ServletContainerInitializer/src/main/java/com/nhnacamemy/student/Controller/StudentUpdateFormController.java
@@ -0,0 +1,25 @@
+package com.nhnacamemy.student.Controller;
+
+import com.nhnacamemy.student.Student;
+import com.nhnacamemy.student.init.RequestMapping;
+import com.nhnacamemy.student.repository.StudentRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
+@RequestMapping(value = "/student/update.do", method = RequestMapping.Method.GET)
+public class StudentUpdateFormController implements Command {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        StudentRepository studentRepository = (StudentRepository) request.getServletContext().getAttribute("studentRepository");
+        String id = request.getParameter("id");
+        Student student = studentRepository.getStudentById(id);
+
+        if (Objects.isNull(student)) {
+            throw new RuntimeException("Student Not Found: " + id);
+        }
+        request.setAttribute("student", student);
+        return "/student/register.jsp";
+    }
+}
