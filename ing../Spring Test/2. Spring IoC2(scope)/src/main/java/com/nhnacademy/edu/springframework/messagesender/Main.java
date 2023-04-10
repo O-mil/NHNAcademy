a5786554b798dcce9d9edce99de5dfb5a3b356cc
O'mil <ghkwjd5343@gmail.com>
2023-04-10 15:56:10 +0900

    spring ioc-scope

---
+++ ing../Spring Test/2. Spring IoC2(scope)/src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.edu.springframework.messagesender;
+
+import com.nhnacademy.edu.springframework.messagesender.sender.MessageSender;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            MessageSender emailMessageSender1 = context.getBean("emailMessageSender", MessageSender.class);
+            MessageSender emailMessageSender2 = context.getBean("emailMessageSender", MessageSender.class);
+
+            MessageSender smsMessageSender1 = context.getBean("smsMessageSender", MessageSender.class);
+            MessageSender smsMessageSender2 = context.getBean("smsMessageSender", MessageSender.class);
+
+
+            User user = new User("ghkwjd5343@gmail.com", "010.5883.6775");
+            emailMessageSender1.sendMessage(user, "email Message");
+            emailMessageSender2.sendMessage(user, "email Message");
+
+            smsMessageSender1.sendMessage(user, "sms Message");
+            smsMessageSender2.sendMessage(user, "sms Message");
+
+        }
+    }
+}
