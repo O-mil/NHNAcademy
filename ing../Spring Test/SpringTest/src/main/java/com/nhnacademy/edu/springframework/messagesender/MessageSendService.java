c5264e51f620b95ea14795c03e67b014b7a4b709
O'mil <ghkwjd5343@gmail.com>
2023-04-11 17:45:09 +0900

    spring 실습

---
+++ ing../Spring Test/SpringTest/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -0,0 +1,32 @@
+package com.nhnacademy.edu.springframework.messagesender;
+
+import com.nhnacademy.edu.springframework.messagesender.sender.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+// 별도로 지정하지 않는 이상은 messageSendService로 설정됨.
+//지정하려면 @Service()안에 지정해주기 -> Spring Bean 이름
+//@Component()도 같음
+@Service ("messageSendService")
+@PropertySource("classpath:name.properties")
+public class MessageSendService {
+
+    private final MessageSender messageSender;
+    @Value("${name}")
+    private String name;
+
+    @Autowired
+    public MessageSendService(@Qualifier("smsMessageSender") MessageSender messageSender) {
+        System.out.println("MessageSender invoked!");
+        this.messageSender = messageSender;
+    }
+
+
+    public void doSendMessage() {
+        User user = new User("ghkwjd5343@gmail.com", "010-5883-6775");
+        messageSender.sendMessage(user,   "hi! " + name);
+    }
+}
