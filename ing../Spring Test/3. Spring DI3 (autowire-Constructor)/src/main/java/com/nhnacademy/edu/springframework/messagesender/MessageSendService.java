0600b177ee01d99e9f63e0c5760f2845c8a9195e
O'mil <ghkwjd5343@gmail.com>
2023-04-11 14:49:21 +0900

    autowire 업로드

---
+++ ing../Spring Test/3. Spring DI3 (autowire-Constructor)/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -0,0 +1,33 @@
+package com.nhnacademy.edu.springframework.messagesender;
+
+import com.nhnacademy.edu.springframework.messagesender.sender.MessageSender;
+
+public class MessageSendService {
+
+    // final keyword 이므로 객체를 생성한뒤 greeter 변수에 값을 할당할 수 없다.
+     private final MessageSender smsMessageSender;
+    private final MessageSender emailMessageSender;
+
+//    private MessageSender messageSender;
+
+    // Setter Injection 기본 생성자가 필요하다.
+    public MessageSendService(MessageSender smsMessageSender, MessageSender emailMessageSender) {
+        this.smsMessageSender = smsMessageSender;
+        this.emailMessageSender = emailMessageSender;
+    }
+
+    public void setSmsMessageSender(MessageSender smsMessageSender) {
+        System.out.println("setSmsMessageSender invoked!");
+    }
+
+    public void setEmailMessageSender(MessageSender emailMessageSender) {
+        System.out.println("setSmsMessageSender invoked!");
+    }
+
+    public void doSendMessage() {
+        User user = new User("ghkwjd5343@gmail.com", "010-5883-6775");
+        smsMessageSender.sendMessage(user,   "hi");
+        emailMessageSender.sendMessage(user,   "hi");
+
+    }
+}
---
+++ ing../Spring Test/3. Spring DI3 (autowire-byName)/src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSendService.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.edu.springframework.messagesender;
+
+import com.nhnacademy.edu.springframework.messagesender.sender.MessageSender;
+
+public class MessageSendService {
+
+    // final keyword 이므로 객체를 생성한뒤 greeter 변수에 값을 할당할 수 없다.
+    // private final MessageSender messageSender;
+    private MessageSender messageSender;
+
+    // Setter Injection 기본 생성자가 필요하다.
+    public MessageSendService() {}
+
+    public void setSmsMessageSender(MessageSender messageSender) {
+        System.out.println("setSmsMessageSender invoked!");
+        this.messageSender = messageSender;
+    }
+
+    public void doSendMessage() {
+        User user = new User("ghkwjd5343@gmail.com", "010-5883-6775");
+        messageSender.sendMessage(user,   "hi");
+    }
+}
