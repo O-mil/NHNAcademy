d0d777b47350c241875d46c0e3cefabc19e996cd
O'mil <ghkwjd5343@gmail.com>
2023-05-18 10:40:49 +0900

    board

--- ing../Security1/demo32/src/main/java/com/example/demo32/SecurityConfig.java
+++ ing../Security1/demo32/src/main/java/com/example/demo32/SecurityConfig.java
@@ -1,2 +1,20 @@
-package com.example.demo32;public class SecurityConfig {
+package com.example.demo32;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers()
+                    .defaultsDisabled()
+                    .cacheControl(c -> c.disable())
+                    .frameOptions().sameOrigin()
+                .and()
+                .build();
+    }
 }
