6b3eac70f0255a63358de1dbf5cddb78b83eec4f
O'mil <ghkwjd5343@gmail.com>
2023-05-19 15:31:56 +0900

    Certifications Entity 맵핑

--- ing../Certifications/src/main/java/org/example/entity/FamilyRelationship.java
+++ ing../Certifications/src/main/java/org/example/entity/FamilyRelationship.java
@@ -1,4 +1,44 @@
 package org.example.entity;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table (name = "family_relationship")
 public class FamilyRelationship {
+
+    @EmbeddedId
+    private Pk pk;
+
+    @Column (name = "family_relationship_code")
+    private String familyRelationshipCode;
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Getter
+    @Embeddable
+    public static class Pk implements Serializable {
+
+        @Column (name = "base_resident_serial_number")
+        private Long baseResidentSerialNumber;
+
+        @Column (name = "family_resident_serial_number")
+        private Long familyResidentSerialNumber;
+    }
+
+    public void modifyRelationInfo(String relationship) {
+        if (familyRelationshipCode != null) {
+            familyRelationshipCode = relationship;
+        }
+    }
 }
--- ing../Certifications/src/main/java/org/example/entity/Household.java
+++ ing../Certifications/src/main/java/org/example/entity/Household.java
@@ -1,4 +1,47 @@
 package org.example.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "household")
 public class Household {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "household_serial_number")
+    private Long householdSerialNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "household_resident_serial_number")
+    private Resident householdResidentSerialNumber;
+
+    @Column(name = "household_composition_date")
+    private LocalDateTime householdCompositionDate;
+
+    @Column(name = "household_composition_reason_code")
+    private String householdCompositionReasonCode;
+
+    @Column(name = "current_house_movement_address")
+    private String currentHouseMovementAddress;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "household", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private  List<HouseholdCompositionResident> householdCompositionResidents;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "household", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private List<HouseholdMovementAddress> householdMovementAddresses;
 }
