3e1721d4893a08dcc92e3adceefa7f8ba9c9d874
O'mil <ghkwjd5343@gmail.com>
2023-05-30 11:34:12 +0900

    JPA-student, account 실습

---
+++ ing../Spring boot/spring-boot-student/src/main/java/com/nhnacademy/springbootstudent/account/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.springbootstudent.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    List<Account> findAll();
+    Optional<Account> findById(Long id);
+}
---
+++ ing../Spring boot/spring-boot-student/src/test/java/com/nhnacademy/springbootstudent/DefaultAccountServiceTest.java
@@ -0,0 +1,29 @@
+package com.nhnacademy.springbootstudent;
+
+import com.nhnacademy.springbootstudent.account.Account;
+import com.nhnacademy.springbootstudent.account.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DefaultAccountServiceTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    void testgetAccounts() {
+        Account one = new Account(1L, "3333", 1000);
+        accountRepository.save(one);
+
+        Optional<Account> actual = accountRepository.findById(1L);
+        assertThat(actual).isPresent();
+        assertThat(actual.get()).isEqualTo(one);
+    }
+}
