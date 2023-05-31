a35781b3be1e3db5b88f361a177cf3a0d54c0c8b
O'mil <ghkwjd5343@gmail.com>
2023-05-31 15:38:46 +0900

    developer tools, spring boot actuator

--- ing../Spring boot/spring-boot-student/src/main/java/com/nhnacademy/springbootstudent/ManagementController.java
+++ ing../Spring boot/spring-boot-student/src/main/java/com/nhnacademy/springbootstudent/ManagementController.java
@@ -1,2 +1,25 @@
-package com.nhnacademy.springbootstudent;public class ManagementController {
+package com.nhnacademy.springbootstudent;
+
+import com.nhnacademy.springbootstudent.actuator.MyHealthIndicator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ManagementController {
+
+    private final MyHealthIndicator myHealthIndicator;
+
+    @PostMapping("/management/fail")
+    public String healthFail() {
+        myHealthIndicator.healthFail();
+        return "health fail";
+    }
+
+    @PostMapping("/management/recover")
+    public String healthRecover() {
+        myHealthIndicator.healthRecover();
+        return "health recovered";
+    }
 }
