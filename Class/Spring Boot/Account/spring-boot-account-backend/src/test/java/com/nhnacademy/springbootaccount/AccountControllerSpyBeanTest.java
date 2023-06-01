46583a6e3c74f0e733785d9211dbb680bbc610f4
O'mil <ghkwjd5343@gmail.com>
2023-06-01 13:51:39 +0900

    SpyBeanTest, AccountRepositoryTest 실습

---
+++ Class/Spring Boot/Account/spring-boot-account-backend/src/test/java/com/nhnacademy/springbootaccount/AccountControllerSpyBeanTest.java
@@ -0,0 +1,49 @@
+package com.nhnacademy.springbootaccount;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AccountControllerSpyBeanTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SpyBean
+    private AccountService accountService;
+
+    @Test
+    @Order(1)
+    void testGetStudents() throws Exception {
+        given(accountService.getAccounts())
+                .will(invocation ->{
+                    System.out.println("Spy!!");
+                    return List.of(new Account(2L, "신짱구", 50));
+                });
+
+        mockMvc.perform(get("/students"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name", equalTo("신짱구")));
+    }
+}
