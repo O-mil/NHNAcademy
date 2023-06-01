c569b8bd39276b7e0cf6009d4120c382f16905fb
O'mil <ghkwjd5343@gmail.com>
2023-06-01 15:18:26 +0900

    spring boot dooray bot 메시지 실습

---
+++ Class/Spring Boot/edu-doorayclient-springboot-workshop/dooray-spring-boot-starter-application/src/main/java/com/nhn/edu/springboot/starter/application/Application.java
@@ -0,0 +1,41 @@
+package com.nhn.edu.springboot.starter.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import com.nhn.dooray.client.DoorayHookSender;
+import com.nhn.dooray.client.DoorayHook;
+
+import java.net.URL;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * TODO (9) @Autowired 로 DoorayHookSender 의존성을 주입합니다.
+     */
+
+    @Autowired
+    private DoorayHookSender doorayHookSender;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+
+    /**
+     * TODO (10) ApplicationReady 일때!, DoorayHookSender 로 Hook 을 발송하는 빈을 선언해 주세요.
+     */
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationListener() {
+        return (event) -> {
+            doorayHookSender.send(DoorayHook.builder()
+                    .botName("hi")
+                    .text("hi")
+                    .build());
+        };
+    }
+}
